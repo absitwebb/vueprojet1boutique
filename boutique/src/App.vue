4240fa74b484240c33bf3db6e003f12deaaf499b
absitwebb <absitwebb@gmail.com>
2022-06-14 11:38:52 +0200

    mise en place

---
+++ boutique/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="b1">bonjour le monde</h1>
+  <div class="essai"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+@import "./assets/base.scss";
+.essai {
+  display: flex;
+  width: 200px;
+  height: 50px;
+  background-color: $colorprimary-1;
+}
+</style>
